37f7280546c842951fe880d6e553dc18c7ec34e4
francescobilla <francesco.billa2004@gmail.com>
2020-11-04 11:11:35 +0100

    Add project files.

---
+++ Operazioni Matematiche/Matematica.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Operazioni_Matematiche
+{
+    class Matematica
+    {
+        public static int Somma(int a,int b)
+        { 
+            
+            return a + b;
+        }
+        public static Double SommaDouble(double a, double b)
+        {
+            return a + b;
+        }
+        public static Double Sottrazione(double a, double b)
+        {
+            return a - b;
+        }
+        public static Double Moltiplicazione(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Divisione(double a, double b)
+        {
+            return a / b;
+        }
+        public static long Potenza(int a,int b)
+        {
+            
+             for (int cont=0 ; cont>b; cont++)
+            {
+                
+                return a * a;
+            }
+        }
+             
+    }
+}
---
+++ Operazioni Matematiche/operazioni Matematiche.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Operazioni_Matematiche
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci un numero:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inserisci un numero:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int somma = Matematica.Somma(a,b);
+            Console.WriteLine($"la somma é:{somma}");
+
+            double sommaDouble = Matematica.SommaDouble(a,b);
+            Console.WriteLine($"la somma double è:{sommaDouble}");
+
+            double sottrazione = Matematica.Sottrazione(a, b);
+            Console.WriteLine($"la sottrazione è:{sottrazione}");
+
+            Double moltiplicazione = Matematica.Moltiplicazione(a, b);
+            Console.WriteLine($"la moltiplicazione è:{moltiplicazione}");
+
+            double divisione = Matematica.Divisione(a,b);
+            Console.WriteLine($"la divisione è:{divisione}");
+            
+        }
+    }
+}
